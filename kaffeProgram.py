d692fffd9f8d8d55435f423b04d9e1a5a4bb7a1f
bawanfn@stud.ntnu.no <bawanfn@stud.ntnu.no>
2022-03-24 16:00:17 +0100

    Noen små korrigeringer

--- kaffeProgram.py
+++ kaffeProgram.py
@@ -3,6 +3,7 @@ import sqlite3
 con = sqlite3.connect("kaffe.db")
 cursor = con.cursor()
 
+
 def login():
     """"Login"""""
     """ ola@nordmann.no  kaffenorge """
@@ -10,18 +11,18 @@ def login():
     print("Vennligst logg inn")
     epost = str(input("E-post: "))
     passord = str(input("Passord: "))
-    cursor.execute("SELECT brukerID, fulltNavn FROM bruker WHERE epost =? AND passord =?;", (epost, passord))
+    cursor.execute(
+        "SELECT brukerID, fulltNavn FROM bruker WHERE epost =? AND passord =?;", (epost, passord))
     brukerdata = cursor.fetchone()
     if (brukerdata):
-        program(brukerdata)    
+        program(brukerdata)
     else:
-        print("\nDet finner ingen bruker som matcher denne kombinasjonen!")
+        print("\nDet finnes ingen bruker som matcher denne kombinasjonen!")
         done = input('\nVil du prøve på nytt? (y/n)')
         if done == 'n':
             exit()
         login()
-        
-    
+
 
 def program(brukerdata):
     """Main program
@@ -33,19 +34,27 @@ def program(brukerdata):
         valg = str(input(""))
         match valg:
             case 'k':
+                kaffe_navn = str(input('Hvilken kaffe smakte du på? '))
+                byggeri_navn = str(input('Hvilket bryggeri kommer den fra? '))
+                poeng = int(input('Hvor mange poeng gir du den?(0-10) '))
+                notat = str(
+                    input('Legg gjerne til et notat som beskriver kaffen: '))
+                # utifra denne dataen skal brukerhistorie 1 oppfylles
                 pass
             case 'd':
+                print('Så spennende med ny data!')
+                valg = str(input('''Hva vil du legge inn data om?\n
+                '''))
                 pass
             case 'x':
                 exit()
-
-
 
 
 def main():
     """ Main """
     login()
     con.close()
-    
+
+
 if __name__ == "__main__":
     main()
