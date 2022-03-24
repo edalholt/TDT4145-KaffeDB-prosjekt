a7f72af656de05ee2fb2aa87acd83aa871885ce4
bawanfn@stud.ntnu.no <bawanfn@stud.ntnu.no>
2022-03-24 17:40:46 +0100

    La til dummy data i kafffebrenneri og ferdigbrent kaffe til brukerhistorie 3

--- tasks.sql
+++ tasks.sql
@@ -7,6 +7,15 @@ ON bruker.brukerID = kaffesmaking.brukerID
 GROUP BY bruker.brukerID
 
 --Brukerhistorie 3
+SELECT  kaffebrenneri.navn AS brennerinavn, ferdigbrendt_kaffe.navn AS kaffenavn, ferdigbrendt_kaffe.kronerPerKg, round((AVG(kaffesmaking.poeng)/ferdigbrendt_kaffe.kronerPerKg )*1000, 2) AS gjennomsnittscore
+FROM kaffesmaking
+JOIN ferdigbrendt_kaffe
+	ON kaffesmaking.kaffeID = ferdigbrendt_kaffe.kaffeID
+JOIN kaffebrenneri
+	ON kaffebrenneri.brenneriID = ferdigbrendt_kaffe.brenneriID
+	
+GROUP by kaffesmaking.kaffeID
+ORDER by gjennomsnittscore  DESC
 
 --Brukerhistorie 4
 
